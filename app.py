a4fc6949810655f08945851272ac34575d2e0a78
marhel28 <ammarmaulahela20@gmail.com>
2026-01-12 20:46:33 +0700

    test

--- app.py
+++ app.py
@@ -13,10 +13,8 @@ from sqlalchemy import String, select
 import os
 from datetime import datetime, timedelta
 
-# ======================
-# CONFIG (ENV ONLY)
-# ======================
-DATABASE_URL = os.getenv("DATABASE_URL")  # postgresql+asyncpg://...
+
+DATABASE_URL = os.getenv("DATABASE_URL")  
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES", "1440"))
